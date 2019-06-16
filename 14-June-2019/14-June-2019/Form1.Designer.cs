d14fd7c5121111cb3b37b908e2a8d066a5077230
BakhtiyarShamilzada <46760730+BakhtiyarShamilzada@users.noreply.github.com>
2019-06-17 01:40:55 +0400

    Add files via upload

---
+++ 14-June-2019/14-June-2019/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _14_June_2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbMarka = new System.Windows.Forms.ComboBox();
+            this.cmbModel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgwMasin = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwMasin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbMarka
+            // 
+            this.cmbMarka.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbMarka.FormattingEnabled = true;
+            this.cmbMarka.Location = new System.Drawing.Point(12, 69);
+            this.cmbMarka.Name = "cmbMarka";
+            this.cmbMarka.Size = new System.Drawing.Size(150, 26);
+            this.cmbMarka.TabIndex = 0;
+            this.cmbMarka.SelectedIndexChanged += new System.EventHandler(this.CmbMarka_SelectedIndexChanged);
+            // 
+            // cmbModel
+            // 
+            this.cmbModel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbModel.FormattingEnabled = true;
+            this.cmbModel.Location = new System.Drawing.Point(174, 68);
+            this.cmbModel.Name = "cmbModel";
+            this.cmbModel.Size = new System.Drawing.Size(150, 26);
+            this.cmbModel.TabIndex = 1;
+            this.cmbModel.SelectedIndexChanged += new System.EventHandler(this.CmbModel_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Marka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(170, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Model";
+            // 
+            // dgwMasin
+            // 
+            this.dgwMasin.BackgroundColor = System.Drawing.Color.White;
+            this.dgwMasin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwMasin.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgwMasin.GridColor = System.Drawing.Color.White;
+            this.dgwMasin.Location = new System.Drawing.Point(0, 100);
+            this.dgwMasin.Name = "dgwMasin";
+            this.dgwMasin.Size = new System.Drawing.Size(909, 350);
+            this.dgwMasin.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.ClientSize = new System.Drawing.Size(909, 450);
+            this.Controls.Add(this.dgwMasin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbModel);
+            this.Controls.Add(this.cmbMarka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwMasin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbMarka;
+        private System.Windows.Forms.ComboBox cmbModel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgwMasin;
+    }
+}
+
---
+++ 14-June-2019/14-June-2019/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace _14_June_2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadMarka();
+            string connString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            using (var conn = new SqlConnection(connString))
+            {
+                string Marka = cmbMarka.Text;
+                string comText = "select Model.Id, Model.Name " +
+                                 "from Model " +
+                                 "join Marka on Model.MarkaId = Marka.Id " +
+                                 $"where Marka.Name = '{Marka}'";
+                using (var comm = new SqlCommand(comText, conn))
+                {
+                    conn.Open();
+                    var reader = comm.ExecuteReader();
+                    if(reader.HasRows)
+                    {
+                        while(reader.Read())
+                        {
+                            cmbModel.Items.Add(reader["Name"]);
+                        }
+                        cmbModel.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        //MessageBox.Show($"There is no Model for this {Marka}");
+                    }
+                }
+            }
+        }
+        void LoadMarka()
+        {
+            cmbMarka.Items.Clear();
+            string connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string commandText = "select * from Marka";
+                using (SqlCommand sqlCommand = new SqlCommand(commandText, conn))
+                {
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    if(reader.HasRows)
+                    {
+                        while(reader.Read())
+                        {
+                            cmbMarka.Items.Add(reader["Name"]);
+                        }
+                        //cmbMarka.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        MessageBox.Show("There is no Marka in the list");
+                    }
+                }
+            }
+        }
+
+        private void CmbMarka_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbModel.Text = "";
+            cmbModel.Items.Clear();
+
+            string connString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            using (var conn = new SqlConnection(connString))
+            {
+                string Marka = cmbMarka.Text;
+                string comText = "select Model.Id, Model.Name " +
+                                 "from Model " +
+                                 "join Marka on Model.MarkaId = Marka.Id " +
+                                 $"where Marka.Name = '{Marka}'";
+                using (var comm = new SqlCommand(comText, conn))
+                {
+                    conn.Open();
+                    var reader = comm.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            cmbModel.Items.Add(reader["Name"]);
+                        }
+                        cmbModel.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        //MessageBox.Show($"There is no Model for this {Marka}");
+                    }
+                }
+            }
+        }
+
+        private void CmbModel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string Model = cmbModel.Text;
+                string commandText = "select Masin.Id, Masin.Name, Marka.Name Marka, Model.Name Model, BanNovu.Name [Ban novu], Reng.Name Reng, Yurus, Qiymet, Seher.Name Seher, YanacaqNovu.Name [Yanacaq Novu], Oturucu.Name Oturucu, SuretQutusu.Name [Suret Qutusu], BuraxilisIli [Buraxilis ili], MuherrikHecmi [Muherrik Hecmi] " +
+                                     "from Masin " +
+                                     "join Model on Masin.ModelId = Model.Id " +
+                                     "join Marka on Model.MarkaId = Marka.Id " +
+                                     "join BanNovu on Masin.BanNovuId = BanNovu.Id " +
+                                     "join Reng on Masin.RengId = Reng.Id " +
+                                     "join Seher on Masin.SeherId = Seher.Id " +
+                                     "join YanacaqNovu on Masin.YanacaqNovuId = YanacaqNovu.Id " +
+                                     "join Oturucu on Masin.OturucuId = Oturucu.Id " +
+                                     "join SuretQutusu on Masin.SuretQutusuId = SuretQutusu.Id " +
+                                     $"where Model.name = '{Model}'";
+                using (SqlCommand command = new SqlCommand(commandText, conn))
+                {
+                    conn.Open();
+                    
+                        SqlDataReader reader = command.ExecuteReader();
+                    
+                        if (reader.HasRows)
+                        {
+                        reader.Close();
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(commandText, conn))
+                        {
+                            DataTable table = new DataTable();
+                            adapter.Fill(table);
+                            dgwMasin.DataSource = table;
+                        }
+                    }
+                        else
+                        {
+                            //MessageBox.Show($"There is no Automobil for this {Model}");
+                        }
+                    
+                        
+                }
+            }
+        }
+    }
+}
